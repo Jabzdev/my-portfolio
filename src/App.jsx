86d85dbacf1d1f9d667d3011d56d5b1c86fe6468
Jabu Himo <abduljabarcodes@gmail.com>
2025-05-18 20:29:27 +0300

    upgraded

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Hero from "./sections/Hero.jsx";
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import Navbar from "./components/NavBar.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <ShowcaseSection />
+            <LogoSection />
+            <FeatureCards />
+            <ExperienceSection />
+            <TechStack />
+            <Testimonials />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+export default App
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {socialImgs} from "../constants/index.js";
+
+const Footer = () => {
+    return (
+        <footer className={"footer"}>
+            <div className={"footer-container"}>
+                <div className={"flex flex-col justify-center md:items-start items-center"}>
+                    <a href={"/"}>Visit my blog</a>
+                </div>
+
+                <div className={"socials"}>
+                    {socialImgs.map((img) => (
+                        <a href={img.link} key={img.link} className={"icon"} target={"_blank"}>
+                            <img src={img.imgPath} alt={img.name} />
+                        </a>
+                    ))}
+                </div>
+
+                <div className={"flex flex-col justify-center"}>
+                    <p className={"text-center md:text-end"}>
+                        © {new Date().getFullYear()} Abdul | JabzDev. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
